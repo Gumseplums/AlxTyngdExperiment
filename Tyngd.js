78a6b30b7c04b7ad097594bf4139f05d1791e1fb
🪻Gumseplums🪻 <102434949+Gumseplums@users.noreply.github.com>
2026-01-29 08:23:26 +0100

    Update Tyngd.js
    back button will now work better

--- Tyngd.js
+++ Tyngd.js
@@ -193,28 +193,24 @@ NextBtn.addEventListener('click', () => {
 });
 
 BackBtn.addEventListener('click', () => {
+    
     if (Step == 1) {
         NextBtn.innerHTML = 'Börja';
         BackBtn.style.display='none';
         ParaText.style.display='block';
         Inst.style.display='none';
 
-        img1.style.display="none"
+        img1.style.display="inline-block"
         img1.src="Images/Bottle.png"
 
-        img2.style.display="none"
+        img2.style.display="inline-block"
         img2.src="Images/Scissors.png"  
         
-        img3.style.display="none"
+        img3.style.display="inline-block"
         img3.src="Images/Tape.png"  
         
-        img4.style.display="none"
+        img4.style.display="inline-block"
         img4.src="Images/Thread.png"
-        
-        
-        InstTitle.innerHTML = 'MATERIAL';
-        InstText.innerHTML = '- Plast Fläska<br>- Sax<br>- Garn eller snöre<br>- Tejp<br><br>(Behöves inte men recommenderas)<br>- Stor pärla<br>- Hot glue pistol<br>- Kniv<br>- Penna';
-
 
         Step --
 
@@ -224,29 +220,77 @@ BackBtn.addEventListener('click', () => {
         InstTitle.innerHTML = 'Steg 1';
         InstText.innerHTML = 'Cut the bottle in half. You will only require the bottom half.<br><br>(This is where using a knife can be easier than a scissor)';
 
+        img1.style.display="none"
+        img1.src="Images/Bottle.png"
+
+        img2.style.display="inline-block"
+        img2.src="Images/BottleHalf.png"  
+        
+        img3.style.display="none"
+        img3.src="Images/Scissors.png"
+        
+        img4.style.display="inline-block"
+        img4.src="Images/Scissors.png"
+
         Step --
 
         console.log(Step);        
     }
     else if (Step == 3) {
         InstTitle.innerHTML = 'Steg 2';
-        InstText.innerHTML = 'Cut the bottle in half.<br>This is where using a knife can be easier than a scissor';
+        InstText.innerHTML = 'Now cut it shorter but leave out 4 strips as "legs" for the bottle.<br>This is where you can use a pen/marker to trace it out. The leg strips should be evenly spaced out.<br><br>Be sure to not make the legs too thin as that will give it bad stability, possibly causing it to fall over too easy. Dont make it too thick either, as the spaces are essential for the model to work.';
 
+        img1.style.display="none"
+        img1.src="Images/Bottle.png"
+
+        img2.style.display="inline-block"
+        img2.src="Images/BottleLegs.png"  
+        
+        img3.style.display="none"
+        img3.src="Images/Scissors.png"
+        
+        img4.style.display="inline-block"
+        img4.src="Images/Scissors.png"
+                
         Step --
 
         console.log(Step);        
     }
     else if (Step == 4) {
         InstTitle.innerHTML = 'Steg 3';
-        InstText.innerHTML = 'Now cut it shorter but leave out 4 strips as "legs" for the bottle.<br>This is where you can use a pem/marker to trace it out. The leg strips should be evenly spaced out.<br><br>Be sure to not make the legs too thin as that will give it really bad stability, possibly causing it to collapse. Dont make it too thick either, as the spaces are essencial for the model to work.';
+        InstText.innerHTML = 'Get the string/yarn and cut two threads of it slightly longer than the diameter of the bottle.';
 
+        img1.style.display="inline-block"
+        img1.src="Images/2 Threads.png"
+
+        img2.style.display="inline-block"
+        img2.src="Images/BottleLegs.png"  
+        
+        img3.style.display="none"
+        img3.src="Images/Scissors.png"
+        
+        img4.style.display="inline-block"
+        img4.src="Images/Scissors.png"
+                        
         Step --
 
         console.log(Step);        
     }
     else if (Step == 5) {
         InstTitle.innerHTML = 'Steg 4';
-        InstText.innerHTML = 'Get the string/yarn and cut two threads of it slightly longer than the diameter of the bottle.';
+        InstText.innerHTML = 'Tape/glue the thread horizontally inside the bottle. Make sure they thread is centered vertically. The two thread will create an X in the middle of the bottle.<br><br> This is Tyngd Punkten.';
+
+        img1.style.display="inline-block"
+        img1.src="Images/BottleStringXTop.png"  
+
+        img2.style.display="inline-block"
+        img2.src="Images/BottleStringX.png"  
+        
+        img3.style.display="inline-block"
+        img3.src="Images/Tape.png"
+        
+        img4.style.display="inline-block"
+        img4.src="Images/Scissors.png"
 
         Step --
 
@@ -254,44 +298,75 @@ BackBtn.addEventListener('click', () => {
     }
     else if (Step == 6) {
         InstTitle.innerHTML = 'Steg 5';
-        InstText.innerHTML = 'Approximately tape/glue the thread horizontally inside the bottle. Make sure they thread is centered. The two thread will create an X in the middle of the bottle.<br><br> This is Tyngd Punkten.';
+        InstText.innerHTML = 'Cut another longer thread of yarn/string. This time it will need to be slightly longer than the length between Tygnd punktet (where the previous threads create an X) and the ground.';
+
+        img1.style.display="inline-block"
+        img1.src="Images/BottleStringXTop.png"  
+
+        img2.style.display="inline-block"
+        img2.src="Images/BottleStringX.png"  
+        
+        img3.style.display="inline-block"
+        img3.src="Images/Thread.png"
+        
+        img4.style.display="inline-block"
+        img4.src="Images/Scissors.png"
 
         Step --
 
         console.log(Step);        
     }
     else if (Step == 7) {
-        InstTitle.innerHTML = 'Steg 6';
-        InstText.innerHTML = 'Cut another thread of yarn/string. This time it will need to be slightly longer than the length between Tygnd punktet (where the previous threads create an X) and the ground';
+        InstTitle.innerHTML = 'Steg 6.5';
+        InstText.innerHTML = 'This is an optional BUT recommended part of the instructions. Tie a bead to the end of the thread to make it heavier. The bead should end up hanging near the ground, but it should NOT touch.';
+
+        img1.style.display="inline-block"
+        img1.src="Images/Thread.png" 
+
+        img2.style.display="inline-block"
+        img2.src="Images/BottleStringX.png"  
+        
+        img3.style.display="inline-block"
+        img3.src="Images/BeadOnThread.png"
+        
+        img4.style.display="inline-block"
+        img4.src="Images/Bead.png"
 
         Step --
 
         console.log(Step);        
     }
     else if (Step == 8) {
-        InstTitle.innerHTML = 'Steg 6.5';
-        InstText.innerHTML = 'This is an optional BUT recommended part of the instructions. Tie a bead to the end of the thread to make it heavier. The bead should just barely be not touching the ground.';
+        InstTitle.innerHTML = 'Steg 7';
+        InstText.innerHTML = 'Tie the newly cut thread to the other two threads, specifically where they meet to make an X. Aka, Tyngd Punkten. Remember that the bead should preferably not touch the ground.';
 
+        img1.style.display="none"
+        img1.src="Images/BottleFinished.png"
+
+        img2.style.display="none"
+        img2.src="Images/BottleFinished.png" 
+        
+        img3.style.display="none"
+        img3.src="Images/Scissors.png"
+        
+        img4.style.display="none"
+        img4.src="Images/BottleFinished.png"
+
+        img5.style.display="inline-block"
+        img5.src="Images/BottleFinished.png"        
+
+        NextBtn.style.display='block'        
         Step --
 
         console.log(Step);        
     }
     else if (Step == 9) {
-        InstTitle.innerHTML = 'Steg 7';
-        InstText.innerHTML = 'Tie the loose thread to the other two threads, specifically where they meet to make an X. Aka, Tyngd Punkten.';
-
-        Step --
-
-        console.log(Step);        
-    }
-    else if (Step = 10) {
         InstTitle.innerHTML = 'Finished!';
-        InstText.innerHTML = 'Youve finished the model! But you are likely wondering what this weird bottle thread mix is. WELL!<br><br>The bottle is the object. The X thread inside the bottle, which is hopefully centered, is just to give the actuall Tyngt Punkten to hang onto. The hanging string is the arrow that shows where the weight is going. So, if Ive done this correctly, when the bead goes outside the object(waterbottle) should loose balance';
+        InstText.innerHTML = 'Youve finished the model! But you are likely wondering what this weird bottle, thread, mix craft is. WELL!<br><br>The bottle is the object. The X thread inside the bottle, which is hopefully centered, is just to give the actual Tyngt Punkten something to hang onto. The hanging string is the "arrow" that shows where the weight is going. So, if Ive done this correctly, when the bead goes outside the object(waterbottle) should loose balance<br> I hope it works, I see no reason why is shouldnt. I may have gotten a liiiiittle overboard with this very simple assignment. I hope I understood the assignment correctly too.<br><br>If I could put in more time into this, I would make it cleaner, fix bugs, add more and better images, and take the effort into writing all this in swedish';
 
+        NextBtn.style.display='none'
         Step --
 
         console.log(Step);        
-    }
-
+    } 
 });
-
